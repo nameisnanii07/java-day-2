c941dae9bf6cbdba99689001316b0f4c28f9fe41
LIKKI NITHIN <likki.nithin2005@gmail.com>
2026-01-31 15:29:36 +0530

    Add files via upload

--- User.java
+++ User.java
@@ -9,7 +9,16 @@ public class User
         System.out.println("ente a value="+a);
         int b = sc.nextInt();
         System.out.println("enter b value="+b);
-        
+        int sum = a+b;
+        System.out.println("The sum="+sum);
+        float c = sc.nextFloat();
+        System.out.println("Enter c value:="+c);
+        double d = sc.nextDouble();
+        System.out.println("Enter d value:="+d);
+        String s =sc.nextLine();
+        System.out.println("Enter s value:="+s);
+
+
     }
 }
     
