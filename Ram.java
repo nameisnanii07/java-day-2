68675658d4e9eaff35f79bd99cb977385a981556
LIKKI NITHIN <likki.nithin2005@gmail.com>
2026-01-31 14:27:19 +0530

    Add files via upload

---
+++ Ram.java
@@ -0,0 +1,12 @@
+public class Ram 
+{
+    public static void main(String[] args) {
+      int a = 18;
+      int b = 48;
+      int c = a + b;
+      System.out.println("print A value=" + a);
+      System.out.print("print B value=" + b);
+      System.out.print("print c value=" + c);
+   }
+}
+    
