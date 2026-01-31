e30bcd98f45e68164b6a670fb02c656e2d55c7fb
LIKKI NITHIN <likki.nithin2005@gmail.com>
2026-01-31 15:03:56 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class User
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("ente a value="+a);
+        int b = sc.nextInt();
+        System.out.println("enter b value="+b);
+        
+    }
+}
+    
+
+
